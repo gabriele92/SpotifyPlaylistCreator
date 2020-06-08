90dc1e1e212173286dc2bfdb16c28c8bb113bbc0
Gabriele Gaudenzi <gabrigaudenzi@gmail.com>
2020-06-08 07:36:30 +0200

    created playlist and added tracks

--- src/main/java/it/gabriele92/playlistcreator/PlaylistCreator.java
+++ src/main/java/it/gabriele92/playlistcreator/PlaylistCreator.java
@@ -1,47 +1,51 @@
 package it.gabriele92.playlistcreator;
-import com.wrapper.spotify.SpotifyApi;
 
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.data.playlists.AddItemsToPlaylistRequest;
+import com.wrapper.spotify.requests.data.playlists.CreatePlaylistRequest;
+import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PlaylistCreator {
 
-   public static void main(String[] args) {
-        SpotifyApi spotifyApi = new SpotifyApi.Builder()
-                .setAccessToken("taHZ2SdB-bPA3FsK3D7ZN5npZS47cMy-IEySVEGttOhXmqaVAIo0ESvTCLjLBifhHOHOIuhFUKPW1WMDP7w6dj3MAZdWT8CLI2MkZaXbYLTeoDvXesf2eeiLYPBGdx8tIwQJKgV8XdnzH_DONk")
-                .build();
+    public static void main(String[] args) {
 
-// Create a request object with the optional parameter "market"
-        final GetSomethingRequest getSomethingRequest = spotifyApi.getSomething("qKRpDADUKrFeKhFHDMdfcu")
-                .market(CountryCode.SE)
-                .build();
+        try {
+            SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                    .setAccessToken("BQAWp6ppOq1ZntlRsX0MJO6w7RysG0vP_VxF1D3UgN4ZAtRY6aC6BH5gwgpEzHsIcEljPC27RxlaRy_3AL_LF3o1DPMGh0wB9pJs_y7gpXmOUfRrMItZhKNGQcgRteHkzoNNzgMawHSm1ixy6wy0LoJ8CiNl2eLijduuFIjm-G9aJCog07cDXem2b03kmKOVsbR4RHgyUvRi_BuLXgg")
+                    .build();
 
-        void getSomething_Sync() {
-            try {
-                // Execute the request synchronous
-                final Something something = getSomethingRequest.execute();
+            final SearchTracksRequest b = spotifyApi.searchTracks("Have a nice day bon jovi").build();
 
-                // Print something's name
-                System.out.println("Name: " + something.getName());
-            } catch (Exception e) {
-                System.out.println("Something went wrong!\n" + e.getMessage());
-            }
+            Paging<Track> paging = b.execute();
+            Track[] list = paging.getItems();
+
+            final CreatePlaylistRequest play = spotifyApi.createPlaylist("1181773944", "Have a nice day").build();
+            Playlist p = play.execute();
+
+            List<String> uris = new ArrayList<>();
+            List<Track> trackList = Stream.of(list).collect(Collectors.toList());
+            trackList.forEach((t) -> {
+                System.out.println(t.getName() + " " + t.getArtists()[0].getName() + " " + t.getAlbum().getName());
+                uris.add(t.getUri());
+            });
+            String[] array = new String[uris.size()];
+
+            AddItemsToPlaylistRequest add = spotifyApi.addItemsToPlaylist(p.getId(), uris.toArray(array)).build();
+            add.execute();
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong!\n" + e.getMessage());
         }
 
-        void getSomething_Async() {
-            try {
-                // Execute the request asynchronous
-                final Future<Something> somethingFuture = getSomethingRequest.executeAsync();
-
-                // Do other things...
-
-                // Wait for the request to complete
-                final Something something = somethingFuture.get();
-
-                // Print something's name
-                System.out.println("Name: " + something.getName());
-            } catch (Exception e) {
-                System.out.println("Something went wrong!\n" + e.getMessage());
-            }
-        }
     }
 
 }
